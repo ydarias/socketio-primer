c8de86fe526ff369b9d748f30ff8bca274dda63f
Yeray Darias <ydarias@gmail.com>
2018-08-27 11:56:35 +0200

    Fixed client socket.io version

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ var io = require("socket.io")(http);
 
 app.get("/", (_req, res) => res.sendFile(__dirname + "/index.html"));
 
-io.on("connection", socket => console.log("a user connected"));
+io.on("connection", socket => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => console.log("user disconnected"));
+});
 
 http.listen(3000, () => console.log("App listening on port 3000"));
