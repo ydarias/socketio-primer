64cf3a793d90e9ee4b641da0b02d8f8f1d0d5b50
Yeray Darias <ydarias@gmail.com>
2018-08-27 12:35:15 +0200

    Send messages to the server

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ io.on("connection", socket => {
   console.log("a user connected");
 
   socket.on("disconnect", () => console.log("user disconnected"));
+
+  socket.on("chat message", msg => console.log(`message: ${msg}`));
 });
 
 http.listen(3000, () => console.log("App listening on port 3000"));
