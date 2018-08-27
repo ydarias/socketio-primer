c215e0c8e3e30ea110188d9ea923c1bd75867ea9
Yeray Darias <ydarias@gmail.com>
2018-08-27 11:38:15 +0200

    Added Socket.io connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-var express = require("express");
-var app = express();
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.get("/", (_req, res) => res.sendFile(__dirname + "/index.html"));
 
-app.listen(3000, () => console.log("App listening on port 3000"));
+io.on("connection", socket => console.log("a user connected"));
+
+http.listen(3000, () => console.log("App listening on port 3000"));
