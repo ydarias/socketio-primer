c8faf00175d1e064b914e26eef77f1b663b445db
Yeray Darias <ydarias@gmail.com>
2018-08-27 10:59:50 +0200

    Added static resources

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = express();
 
-app.get("/", (_req, res) => {
-  res.send("Hello world");
-});
+app.get("/", (_req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.listen(3000, () => console.log("App listening on port 3000"));
